6782d45628628adab4b8dd2c34d7e2f88ee1ab8d
Arafat Hossain <arafat6462@gmail.com>
2022-12-07 11:11:21 +0600

    remove firebase,table style warnings

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-  
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import TabNavigation from './app/components/navigation/TabNavigation';
- 
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import TabNavigation from "./app/components/navigation/TabNavigation";
+import { LogBox } from "react-native";
+
 export default function App() {
+  LogBox.ignoreLogs([
+    "Invalid prop `textStyle` of type `array` supplied to `Cell`",
+  ]);
   return (
     <NavigationContainer>
-      <TabNavigation/> 
+      <TabNavigation />
     </NavigationContainer>
-    
   );
 }
-
- 
