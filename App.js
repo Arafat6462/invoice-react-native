afc27938961bb78270de646f291bff12f55517f8
Arafat Hossain <arafat6462@gmail.com>
2022-12-07 16:45:30 +0600

    depen..

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import TabNavigation from "./app/components/navigation/TabNavigation";
 import { LogBox } from "react-native";
+import "react-native-gesture-handler";
 
 export default function App() {
   LogBox.ignoreLogs([
